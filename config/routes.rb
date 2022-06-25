2e4e099c9bcedf9cbee6b67fb53ca538690e1889
salmaqnsgt <nurussalamahqonaah@gmail.com>
2022-06-25 19:47:12 +0700

    Implemented layout for homepage and nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'static#homepage'
   resources :posts
   devise_for :users
-  root to: 'static#homepage'
+  # devise_for :users do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
 end
