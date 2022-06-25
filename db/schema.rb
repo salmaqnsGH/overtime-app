9d459c5457b52f2e424dbc432ead9359e49dde55
salmaqnsgt <nurussalamahqonaah@gmail.com>
2022-06-25 15:40:29 +0700

    Implemented user connection with tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_24_070101) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_25_081235) do
   create_table "posts", force: :cascade do |t|
     t.date "date"
     t.text "rationale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_070101) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
