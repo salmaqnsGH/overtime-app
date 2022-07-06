abc271372d13c3652c3ea3d12062e973b69bc026
salmaqnsgt <nurussalamahqonaah@gmail.com>
2022-07-06 11:17:06 +0700

    Removed registration functionality from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static#homepage'
   resources :posts
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   # devise_for :users do
   #   get '/users/sign_out' => 'devise/sessions#destroy'
   # end
