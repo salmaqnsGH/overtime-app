0f370cdf6d56465c5321bd285b7cb67cf42ba6a8
salmaqnsgt <nurussalamahqonaah@gmail.com>
2022-07-06 11:32:35 +0700

    Implemented administrate admin dashboard"

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'bootstrap', '~> 5.1.3'
 gem 'jquery-rails'
 gem "autoprefixer-rails"
+gem "administrate"
+gem 'bourbon'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :posts
+      resources :users
+      resources :admin_users
+
+      root to: "posts#index"
+    end
   root to: 'static#homepage'
   resources :posts
   devise_for :users, skip: [:registrations]
